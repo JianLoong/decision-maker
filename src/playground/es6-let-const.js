496d63a9ac79451f48d6f0fd1e153ba2a2c72887
Jian Liew <jianloongliew@gmail.com>
2018-11-04 22:08:25 +1100

    Initial commit

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,20 @@
+var nameVar = 'Jian';
+var nameVar = 'Pelo';
+console.log('nameVar', nameVar);
+
+// Let based variable
+//let nameVar = "Jian";
+console.log('nameVar', nameVar);
+
+//let letVar = "Test"
+//let letVar = "Test";
+
+const test = "Test";
+const test = "Test";
+
+// Var based variables are function scoped
+function getName(){
+    var scoping = "Test";
+}
+
+console.log(scoping);
---
+++ src/playground/indecision.js
@@ -0,0 +1,76 @@
+console.log("App.js is running");
+
+const app = {
+  title: "My Indecision App",
+  subtitle: "This is the subtitle",
+  options: []
+};
+
+const removeAll = () => {
+  console.log("Remove all");
+  app.options = [];
+  renderTemplate();
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+  //console.log('Form submit');
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    console.log(option);
+    renderTemplate();
+  }
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  console.log(randomNum);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+// JSX - JavaScript XML (JavaScript syntax extension)
+
+const appRoot = document.getElementById("app");
+
+const numbers = [1, 3, 4, 5, 6];
+
+const renderTemplate = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>
+        {app.options && app.options.length > 0
+          ? "Here are your options"
+          : "No Options : "}
+      </p>
+      <button disabled={app.options.length === 0? true : false} onClick={onMakeDecision}>What should I do?</button>
+      <p>{app.options.length}</p>
+      <ol>
+      {
+        app.options.map((option) => <li key={option}>{option}</li>)
+      }
+      </ol>
+      {/*
+        numbers.map((number) => {
+          return <p key={number}>Number : {number}</p>
+        })
+      */}
+      {/* onSubmit is a reference not method*/}
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+        <button onClick={removeAll}>Remove all</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderTemplate();
