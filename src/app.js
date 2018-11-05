5fac3b9ae5fdde13376b146bb9cd897d9f6a5704
Jian Liew <jianloongliew@gmail.com>
2018-11-05 17:45:09 +1100

    Updated to stateless functional components

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
+import './styles/styles.css'
 
 //Default props can be used to achieve more reusable components.
 ReactDOM.render(<IndecisionApp options={[]} />, document.getElementById("app"));
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-const Option = props => {
-  return (
-    <div>
-      {props.option}
-      <button
-        onClick={e => {
-          props.handleDeleteOption(props.option);
-        }}
-      >
-        Delete
-      </button>
-    </div>
-  );
-};
+const Option = props => (
+  <div>
+    {props.option}
+    <button
+      onClick={e => {
+        props.handleDeleteOption(props.option);
+      }}
+    >
+      Delete
+    </button>
+  </div>
+);
 
 export default Option;
