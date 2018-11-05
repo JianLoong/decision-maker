371c318724f48a7c401eaead7a3dbf170428bb42
Jian Liew <jianloongliew@gmail.com>
2018-11-05 19:04:08 +1100

    Cleaned up gh pages

--- src/app.js
+++
@@ -1,8 +0,0 @@
-// install -> import -> use
-import React from "react";
-import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
-import './styles/styles.css'
-
-//Default props can be used to achieve more reusable components.
-ReactDOM.render(<IndecisionApp options={[]} />, document.getElementById("app"));
